9a54002ba9269d56b10e7e35bfa717a7dc20b5ae
Antero Duarte <a.fduarte1@gmail.com>
2016-02-20 17:23:40 +0000

    Added mode 3, continuous input mode and using search.h now

--- search.h
+++ search.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <ctype.h>
+
+#define BUFF_SIZE  512*sizeof(char)
+
 struct opts{
     // Mode: 
     //  1 = literal search (e.g. if you search "hello", you get one occurence for each time "hello" appears in the output file)
@@ -12,8 +16,64 @@ struct opts{
     int output_lines;
 };
 
-//TODO: htmlify if output to file 
-//TODO: Whole word checking not working
+//STATUS: modes 1&3 working, lacking mode 2 and multi word search
+//TODO: Implement inclusive search with strstr
+
+int find_match(const char *haystack, const char *needle, int mode){
+
+    // Returns a struct that holds the result (1 or 0) and an int that represents the offset from the first character where the first match happened
+    // The char where the first match happened should only be accounted for if the result is 1
+
+    // j keeps track of how many characters have been checked
+    int j = 0;
+    // matches keeps track of how many matches were found
+
+    if(mode==3){
+        if((strstr(haystack,needle)) != NULL){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    for(int i =0;i<strlen(haystack);i++){
+        if(haystack[i]==needle[j]){
+            j++;
+        }else{
+            return 0;
+        }
+    }
+
+    return 1;
+
+    
+}
+
+int find_match_nocase(const char *haystack, const char *needle, int mode){
+    char *temp_haystack;
+    char *temp_needle;
+    int r;
+
+    temp_haystack=(char*)malloc((strlen(haystack))*sizeof(char));
+    strcpy(temp_haystack,haystack);
+
+    temp_needle=(char*)malloc((strlen(needle))*sizeof(char));
+    strcpy(temp_needle,needle);
+
+    for(int i = 0; i<(strlen(temp_needle)-1); i++){
+        temp_needle[i] = (char)tolower(temp_needle[i]);
+    }
+
+    for(int i = 0; i<(strlen(temp_haystack)-1); i++){
+        temp_haystack[i] = (char)tolower(temp_haystack[i]);
+    }
+
+    r = find_match(temp_haystack,temp_needle,mode);
+    free(temp_haystack);
+    free(temp_needle);
+
+    return r;
+}
 
 // Search function
 int search(char *string, char *input, char *output, struct opts options){
@@ -22,17 +82,21 @@ int search(char *string, char *input, char *output, struct opts options){
     //Output file pointer
     FILE *ofp;
     int line_num = 1;
-    int find_result = 0;
+    int matches = 0;
     
     char *buffer;
 
-    char *word = NULL;
+    char *word;
 
     //Open input file
     //If file fails to open, output message and return 0 for unsuccessful
-    if((ifp = fopen(input, "r")) == NULL){
+    //strcmp() returns 0 if the strings are equal.
+    if(!strcmp(input,"stdin")){
+        ifp=stdin;
+    //Else, open file output, if file fails to open, output message and return 0 for unsuccessful
+    }else if((ifp = fopen(input, "r")) == NULL){
         printf("Can't open input file: %s\n",input);
-        return 0;
+        return (-1);
     }else{
         printf("Opened file: %s to read from\n",input);
     }
@@ -43,113 +107,113 @@ int search(char *string, char *input, char *output, struct opts options){
         ofp=stdout;
     //Else, open file output, if file fails to open, output message and return 0 for unsuccessful
     }else if((ofp=fopen(output, "w"))==NULL){
-        //if(DEBUG)printf("Can't open output file: %s\n",output);
-        return 0;
-    //Else, print success message
+        printf("Can't open output file: %s\n",output);
+        return (-1);
     }else{
-        //if(DEBUG)printf("Opened file: %s to write to\n",output);
+        printf("Opened file: %s to write to\n",output);
     }
 
     // Print the result separator
     printf("\n--RESULTS--\n\n");
 
-    buffer = malloc(512*sizeof(char));
+    char c;
 
-    while(fgets(buffer, 512, ifp) != NULL){
+    buffer = (char*)malloc(BUFF_SIZE);
 
-        printf("Pointer to buffer: %p\n", buffer);
+    // Flag for continuous input
+    int cont_input = 1;
 
+    int newline;
+
+    // Declare the result variable outside the loop so it isn't reallocated for every iteration
+    int result;
+
+    if(options.mode !=2){
         do{
+            c = fscanf(ifp,"%s",buffer);
 
-            if(strlen(buffer)<strlen(string)){
-                printf(" remaining buffer is smaller than string\n");
-                word = NULL;
-            }else{
-                
-                printf("Gonna search for word now\n");
-
-                // If case sensitive, do strstr, else do strcasestr
-                word = options.match_case?strstr(buffer, string):strcasestr(buffer, string);
-
-
-                if(word != NULL)
-                    printf("Pointer to word: %p\n", word);
-
-                if(word!=NULL && word[strlen(string)] != '\n' || word[strlen(string)] != '\0'){
-                    printf("Gonna change the buffer pointer\n");
-                    buffer = word+strlen(string)-1;
-                    printf("New pointer to buffer: %p\n", buffer);
-                    printf("New buffer: %s\n", buffer);
+            if(!strcmp(input,"stdin")){
+            // Check if text should be asked for in continuous mode, strcmp returns 0 if the strings match
+                if(!strcmp(buffer,"--quit--")){
+                    cont_input=0;
                 }
-
-
-            //If output_lines, print the line number before the line
-                if(options.output_lines)
-                    fprintf(ofp,"Line %d:", line_num);
-
-            //if(DEBUG) fprintf(ofp, "\n\nThe line is:%s\nThe Last char is:%d\n", buffer,(int)buffer[strlen(buffer)-1]);
-
-             // Check if:
-                 // " word " - leading and trailing spaces, meaning its a single word and not a substring of a bigger word
-                 // or "\nword " - is the first word in the line (address of word[0]==address of line[0])
-                 // or " word\n" - is the last word in the buffer (leading space and trailing line end)
-                 // or "\nword\n" - is the last word in the buffer
-                     // Before string rules: space, or address of word[0]== address of buffer[0] -> word==line || *(word-1)==" "
-                     // After string rules: space or '\n' -> *(word+strlen(word))==" " || *(word+strlen(word))=="\n"
-                     // If one of each applies, word is not substring of larger string.
-
-                if(word==buffer && strcmp((word+strlen(string))," ") || strcmp((word-1)," ") && strcmp((word+strlen(string))," ") || strcmp((word-1)," ") && word[strlen(string)]=='\n'){
-                    if(options.mode==1){
-                        word[strlen(string)] ='\0';
-                        fprintf(ofp,"%s ",word);
-                        fprintf(ofp, "\n");
-                    }else if(options.mode==2){
-                        fprintf(ofp,"%s ",buffer);
-                    }
-                }
-
-                if(options.mode>2){
-                    if(options.mode==3){
-                        word[strlen(string)] ='\0';
-                        fprintf(ofp,"%s ",word);
-                    }else if(options.mode==4){
-                        fprintf(ofp,"%s ",buffer);
-                    }
-                }
-
-                find_result++;
-
-                if(buffer[strlen(buffer)-1]== '\n'){
-                    printf("\n");
-                }
-
             }
 
-        }while(word!=NULL);
+            // There's no point in searching if the needle is bigger than the haystack
+            if(strlen(buffer)>strlen(string)){
+                result = options.match_case?find_match(buffer,string,options.mode):find_match_nocase(buffer,string,options.mode);
+            }
 
-        if(buffer[strlen(buffer)-1]== '\n'){
+            // If a match is found
+            if(result){
+                // If we're reading from stdin
+                if(!strcmp(input,"stdin")){
+                // This makes it easier to see tha match on the command line
+                    printf("Match:");
+                }
+
+                fprintf(ofp,"%s\n",buffer);
+
+                // Increase the number of matches
+                matches++;
+            }
+
+        }while (c != EOF && cont_input == 1);
+
+    }else if(options.mode == 2){
+
+        word = (char*)malloc(BUFF_SIZE);
+
+        while(fgets(buffer, 512, ifp) != NULL){
+
+            while(sscanf(buffer,"%s%n",word,&newline) == EOF){
+                result = options.match_case?find_match(word,string,options.mode):find_match_nocase(word,string,options.mode);
+
+                if(result){
+                    // If we're reading from stdin
+                    if(!strcmp(input,"stdin")){
+                    // This makes it easier to see tha match on the command line
+                        printf("Match:");
+                    }
+
+                    if(options.output_lines){
+                        fprintf(ofp, "Line %d:",line_num);
+                    }
+
+                    fprintf(ofp,"%s\n",buffer);
+
+                    // Increase the number of matches
+                    matches++;
+                }
+            }
+
             line_num++;
         }
 
+        free(word);
 
+    }else{
+        printf("Error parsing mode\n");
+        return 0;
     }
-
-
 
     free(buffer);
 
-    //Close the files if still open.
-        if(ifp) 
-            fclose(ifp);
 
-        if(ofp && !strcmp(output,"stdout"))
-            fclose(ofp);
-
-        if(find_result == 0) {
-            printf("\nNo matches found for %s in %s.\n",string,input);
-        }else{
-            printf("\n%d Matches found.\n", find_result);
-        }
-
-        return find_result;
+    if(matches == 0){
+        fprintf(ofp,"\nNo matches found for %s in %s.\n",string,input);
+    }else{
+        fprintf(ofp,"\n%d Matches found.\n", matches);
     }
+
+    //Close the files if still open.
+    if(ifp){
+        fclose(ifp);
+    }
+
+    if(ofp && strcmp(output,"stdout")){
+        fclose(ofp);
+    }
+
+    return matches;
+}
